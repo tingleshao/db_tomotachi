fda6f401693b4c76e6181e50a4f4bdad2f5564c6
my ubuntu <tingleshao@gmail.com>
2013-01-25 13:53:16 -0500

    !

--- main.rb
+++ main.rb
@@ -1,4 +1,16 @@
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
 puts 'hello world'
+
+
+user_id = ""
+if ARGV.size >= 1
+  user_id = ARGV[0]
+end 
+
+puts "user_id: " + user_id
+.'
+
+
